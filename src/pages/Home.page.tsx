a55c19bc2cda811507baacd7be0647ed2a753bbc
VilliqTech <dev-team@villiq.org>
2025-05-13 20:35:51 -0400

    chore: Update prettierrc file so that it is recognized by vscode. Create initial theme

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,29 @@
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export function HomePage() {
+    const [opened, { toggle }] = useDisclosure();
+
     return (
-        <>
-            <Welcome />
-            <ColorSchemeToggle />
-        </>
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: { mobile: !opened },
+            }}
+            padding='md'
+        >
+            <AppShell.Header p='lg'>
+                <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm' />
+                <div>Logo</div>
+            </AppShell.Header>
+
+            <AppShell.Navbar p='lg'>Navbar</AppShell.Navbar>
+
+            <AppShell.Main>Main</AppShell.Main>
+        </AppShell>
     );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,102 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-    /** Put your mantine theme override here */
+    colors: {
+        primary: [
+            '#cdb0e8',
+            '#c09ce2',
+            '#b488dd',
+            '#a774d7',
+            '#7f38c2',
+            '#7533b2',
+            '#682e9e',
+            '#5b288b',
+            '#4e2277',
+            '#34174f',
+        ],
+        gradientFrom: [
+            '#cdb0e8',
+            '#c09ce2',
+            '#b488dd',
+            '#a774d7',
+            '#7f38c2',
+            '#7533b2',
+            '#682e9e',
+            '#5b288b',
+            '#4e2277',
+            '#34174f',
+        ],
+        gradientTo: [
+            '#fb9ded',
+            '#fa84e8',
+            '#f96ce3',
+            '#f853df',
+            '#f50ad1',
+            '#e609c4',
+            '#c408a7',
+            '#ac0792',
+            '#93067d',
+            '#620454',
+        ],
+    },
+    primaryShade: 4,
+    primaryColor: 'primary',
+    headings: {
+        fontFamily: 'Roboto, sans-serif',
+        sizes: {
+            h1: { fontSize: 36 },
+        },
+    },
+    defaultRadius: 'md',
+    shadows: {
+        md: '1px 1px 3px rgba(0, 0, 0, .25)',
+        xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+    },
+    respectReducedMotion: true,
+    components: {
+        Button: {
+            styles: {
+                root: {
+                    borderWidth: 2,
+                },
+            },
+        },
+        Chip: {
+            styles: {
+                label: {
+                    borderWidth: 2,
+                },
+            },
+        },
+        Input: {
+            styles: {
+                input: {
+                    borderWidth: 2,
+                },
+            },
+        },
+        Pagination: {
+            styles: {
+                item: {
+                    borderWidth: 2,
+                },
+            },
+        },
+        Switch: {
+            styles: {
+                track: {
+                    borderWidth: 2,
+                },
+            },
+        },
+    },
+    other: {
+        defaultBorderWidth: 2,
+        buttonActiveStyle: 'zoom',
+    },
+    defaultGradient: {
+        deg: 45,
+        to: 'gradientTo',
+        from: 'gradientFrom',
+    },
 });
